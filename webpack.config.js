0237c1cc96a0c09d5675d2ac2be74e1bb1696d99
Andrey <korsap@bk.ru>
2018-03-06 23:07:43 +0500

    123

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 module.exports = {
 	entry: {
@@ -6,6 +10,47 @@ module.exports = {
 	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: '[name].js'
-	}
+		filename: 'js/[name].js'
+	},
+/*	watch: true,
+	watchOptions: {
+		aggregateTimeout: 300,
+		poll: 1000
+	},*/
+	stats: {
+		colors: true,
+		modules: true,
+		reasons: true,
+		errorDetails: true
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Вы все говно',
+			filename: 'index.html',
+			inject: true,
+			hash: true,
+			template: 'index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true
+			}
+		}),
+		new webpack.WatchIgnorePlugin([
+			path.join(__dirname, 'node_modules')
+		]),
+		new ProgressBarPlugin(),
+		new FriendlyErrorsPlugin({
+			compilationSuccessInfo: {
+				messages: ['You application is running here: http://localhost:8080'],
+				notes: ['Some additional notes']
+			},
+			onErrors: function (severity, errors) {
+				console.log("severity=", severity)
+				console.log("errors=", errors)
+			},
+			clearConsole: true
+		})
+	]
 }
