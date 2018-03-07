d2171922561da5241dfd8344724ece99b78f691f
Andrey Savchenko <korsap@bk.ru>
2018-03-07 13:46:43 +0500

    styles loaders was added

--- src/component.js
+++ src/component.js
@@ -1,5 +1,8 @@
+import style from './assets/styles.css'
+
 export default (text = 'Hello world!') => {
 	const element = document.createElement('div')
 	element.innerHTML = '<p>' + text +'</p>'
+	element.className = style.greenClass
 	return element
 }
